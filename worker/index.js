81d1a4705b1884145a3331a8490df4be9a6d3118
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-06-20 15:13:25 +0530

    Add redis config

--- worker/index.js
+++ worker/index.js
@@ -1,1 +1,11 @@
 const keys = require('./keys');
+const redis = require('redis');
+const { redisPort } = require('./keys');
+
+const redisClient = redis.createClient({
+	host: keys.redisHost,
+	port: redisPort,
+	retry_strategy: () => 1000,
+});
+
+const sub = redisClient.duplicate();
